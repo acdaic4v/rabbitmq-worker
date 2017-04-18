9a448f592d7e28cdc4fbcd48d6c482509ec2208b
acdaic4v <acdaic4v@users.noreply.github.com>
2017-04-18 13:27:00 +0200

    elsif -> if

--- rabbitmq-worker.pl
+++ rabbitmq-worker.pl
@@ -35,47 +35,50 @@ for my $eintrag (@$yamlconfig)
         {
                 $mq_server = $eintrag->{mq_server};
         }
-        elsif($eintrag->{mq_connection})
+        if($eintrag->{mq_connection})
         {
                 $mq_connection = $eintrag->{mq_connection};
         }
-        elsif($eintrag->{mq_user})
+        if($eintrag->{mq_user})
         {
                 $mq_user = $eintrag->{mq_user};
         }
-        elsif($eintrag->{mq_password})
+        if($eintrag->{mq_password})
         {
                 $mq_password = $eintrag->{mq_password};
         }
-        elsif($eintrag->{mq_vhost})
+        if($eintrag->{mq_vhost})
         {
                 $mq_vhost = $eintrag->{mq_vhost};
         }
-        elsif($eintrag->{mq_channel})
+        if($eintrag->{mq_channel})
         {
                 $mq_channel = $eintrag->{mq_channel};
         }
-        elsif($eintrag->{mq_exchange})
+        if($eintrag->{mq_exchange})
         {
                 $mq_exchange = $eintrag->{mq_exchange};
         }
-        elsif($eintrag->{mq_queuename})
+        if($eintrag->{mq_queuename})
         {
                 $mq_queuename = $eintrag->{mq_queuename};
         }
-        elsif($eintrag->{mq_routing_key})
+        if($eintrag->{mq_routing_key})
         {
                 $mq_routing_key = $eintrag->{mq_routing_key};
         }
 }
-
 my ($log, $mq);
 $mq = Net::RabbitMQ->new;
 
+print "CONNECT: $mq_server, { user => $mq_user, password => $mq_password, vhost => $mq_vhost }\n";
 $mq->connect($mq_server, { user => $mq_user, password => $mq_password, vhost => $mq_vhost })
   or die "Can't connect to RabbitMQ\n";
+  print "OPEN: $mq_channel \n";
 $mq->channel_open($mq_channel);
+print "EXCHANGE: $mq_channel, $mq_exchange\n";
 $mq->exchange_declare($mq_channel, $mq_exchange);
+print "DECLARE: $mq_channel, $mq_queuename\n";
 $mq->queue_declare($mq_channel, $mq_queuename, {
     passive => 0,
     durable => 1,
@@ -93,7 +96,7 @@ while(1)
         my $rv = $mq->recv();
         print "BODY: $rv->{body}\n";
         # Your Action: Do what you have to do with the body:
-        my $rc = system("/usr/local/bin/worker.pl $rv->{body}");
+        my $rc = system("/usr/local/bin/myworker/worker.pl $rv->{body}");
         print "RC: $rc\n";
         open FILE, ">/opt/transfer/rabbitworker.txt" or die "Cannot open logfile";
         print FILE "WORKER: $rc: $rv->{body}\n";
